199beca26c130f918d659ecf0ae5537a8cc9d34c
Md Ataullah <ataullahm100@gmail.com>
2024-07-09 15:33:26 +0600

    fdf

--- src/Components/ThemeCard/ThemeCard.jsx
+++ src/Components/ThemeCard/ThemeCard.jsx
@@ -4,14 +4,14 @@ import Link from "next/link";
 import React from "react";
 
 const ThemeCard = ({ dta }) => {
-  // console.log(dta);
-  const {
-    name: themeName = "",
-    includeSupport,
-    price,
-    featuredPhoneImage,
-    featuredDesktopImage,
-  } = dta;
+  console.log(dta);
+  // const {
+  //   name: themeName = " ",
+  //   includeSupport,
+  //   price,
+  //   featuredPhoneImage,
+  //   featuredDesktopImage,
+  // } = dta;
   return (
     <div className="rounded-md shadow-md shadow-slate-200">
       {/* Image */}
@@ -19,13 +19,13 @@ const ThemeCard = ({ dta }) => {
         <div
           className="relative h-80 bg-cover bg-top bg-no-repeat w-full rounded-t-md"
           style={{
-            backgroundImage: `url(${featuredDesktopImage})`,
+            backgroundImage: `url(${dta?.featuredDesktopImage})`,
           }}
         >
           <div
             className="h-56 w-36 bg-cover bg-top bg-no-repeat rounded-t-md absolute bottom-0 right-3 border-2 border-slate-900"
             style={{
-              backgroundImage: `url(${featuredPhoneImage})`,
+              backgroundImage: `url(${dta?.featuredPhoneImage})`,
             }}
           ></div>
         </div>
@@ -33,9 +33,9 @@ const ThemeCard = ({ dta }) => {
 
       <div className="p-3">
         <div className="flex items-center justify-between">
-          <h1 className="text-xl font-semibold">{themeName}</h1>
+          <h1 className="text-xl font-semibold">{dta?.name}</h1>
           <h1 className="text-xl font-semibold">
-            ${price} <span className="text-base">USD</span>
+            ${dta?.price} <span className="text-base">USD</span>
           </h1>
         </div>
 
@@ -47,7 +47,7 @@ const ThemeCard = ({ dta }) => {
         </div>
         {/*  */}
         <div className="flex flex-wrap gap-2 my-5">
-          {includeSupport.map((dta, i) => (
+          {dta?.includeSupport.map((dta, i) => (
             <button
               key={i}
               className="px-2 py-1 text-sm bg-slate-100 rounded-md font-medium"
