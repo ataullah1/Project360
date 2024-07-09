eae387fe595961d2e59350b193459df976b8355e
tafiya <tafiapinkey@gmail.com>
2024-07-09 09:49:14 +0600

    add data in nested route

--- src/app/collections/largeCatalogs/page.jsx
+++ src/app/collections/largeCatalogs/page.jsx
@@ -1,11 +1,24 @@
 import Header from "@/Components/HeadingComponents/Header";
+import ThemeCard from "@/Components/ThemeCard/ThemeCard";
 import React from "react";
 
 const LargeCatalogs = () => {
   return (
     <div className="mt-24">
-      <Header heading ={"Large catalogs"} 
-       details={"Explore themes that make it easy for customers to find the right product. Features to look for: enhanced search and filtering, mega menus, breadcrumbs, and collection page navigation"} />
+      <Header
+        heading={"Large catalogs"}
+        details={
+          "Explore themes that make it easy for customers to find the right product. Features to look for: enhanced search and filtering, mega menus, breadcrumbs, and collection page navigation"
+        }
+      />
+      {/* All Card Section */}
+      <div className="py-8 md:py-12 w-11/12 mx-auto  items-center justify-between">
+        <div className="grid grid-cols-1 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+          {[1, 2, 3, 4, 5, 6, 7, 8].map((element) => (
+            <ThemeCard key={1} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
--- src/app/collections/minimalist/page.jsx
+++ src/app/collections/minimalist/page.jsx
@@ -1,18 +1,21 @@
-import Header from '@/Components/HeadingComponents/Header';
-import React from 'react';
+import Header from "@/Components/HeadingComponents/Header";
+import ThemeCard from "@/Components/ThemeCard/ThemeCard";
+import React from "react";
 
 const Minimalist = () => {
-    return (
-        <div className='mt-24'>
- <Header
-       heading ={"Minimalist style"}
-        details={
-          ""
-        }
-      />
-        
-          </div>
-    );
+  return (
+    <div className="mt-24">
+      <Header heading={"Minimalist style"} details={""} />
+      {/* All Card Section */}
+      <div className="py-8 md:py-12 w-11/12 mx-auto  items-center justify-between">
+        <div className="grid grid-cols-1 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+          {[1, 2, 3, 4, 5, 6, 7, 8].map((element) => (
+            <ThemeCard key={1} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Minimalist;
