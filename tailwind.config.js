a7356a95652bcfc99b46202e48f137a027a330c5
Md Ataullah <ataullahm100@gmail.com>
2024-07-10 09:39:46 +0600

    dfs

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,12 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      theme: {
+        colors: {
+          primaryColor: "#FF4719",
+          secColor: "#0067F0",
+        },
+      },
     },
   },
   plugins: [nextui()],
