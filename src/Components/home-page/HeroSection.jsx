56ea9f2164479a79af17114904148bdf77f989d8
Md Ataullah <ataullahm100@gmail.com>
2024-07-10 16:48:46 +0600

    Today Work  done

--- src/Components/home-page/HeroSection.jsx
+++ src/Components/home-page/HeroSection.jsx
@@ -52,7 +52,7 @@ const HeroSection = () => {
               spaceBetween={30}
               centeredSlides={true}
               autoplay={{
-                delay: 2500,
+                delay: 2000,
                 disableOnInteraction: false,
               }}
               modules={[Autoplay]}
@@ -80,7 +80,7 @@ const HeroSection = () => {
               spaceBetween={30}
               centeredSlides={true}
               autoplay={{
-                delay: 2500,
+                delay: 2000,
                 disableOnInteraction: false,
               }}
               modules={[Autoplay]}
