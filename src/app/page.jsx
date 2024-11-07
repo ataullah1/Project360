5496070491b5aa81b9f97feb26446e22735526b3
Md Ataullah <ataullahm100@gmail.com>
2024-11-07 22:46:03 +0600

    ..

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -17,6 +17,7 @@ export default function Home() {
       </div>
       <div className="flex items-center justify-center mb-9 text-white">
         <PDFGenerator />
+        <PDFGenerator />
       </div>
       <Testimonial />
       <HomeMarquee />
